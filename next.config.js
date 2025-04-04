7ee9de958220b2ac2dda97fefb422ccb32a36dfe
cherrera0001 <herrera.jara.cristobal@gmail.com>
2025-04-04 20:31:16 +0000

    fix: actualiza estilos y configuración de build

---
+++ next.config.js
@@ -0,0 +1,8 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    output: 'export', // necesario para que funcione el export estático
+    reactStrictMode: true,
+  }
+  
+  module.exports = nextConfig
+  
